884c21676fab555cb72930ab0eaca5139f77a63d
probitie <viacheslav.mashkov@gmail.com>
2022-12-09 11:47:40 +0200

    file storage contract

--- MyContract.sol
+++ MyContract.sol
@@ -1,13 +1,45 @@
 pragma solidity ^0.8.0;
 
-contract MyContract {
-    uint value = 1;
-
-    function get() public view returns (uint){
-        return value;
+contract Proof
+{
+    struct FileDetails
+    {
+        uint timestamp;
+        string owner;
     }
 
-    function double() public {
-        value *= 2;
+    mapping (string => FileDetails) files;
+    event logFileAddedStatus(bool status, uint timestamp, string owner,
+    string fileHash);
+
+    //функция для сохранения владельца файла и метки времени блока
+    function set(string memory owner, string memory fileHash) public
+    {
+        //не существует корректного способа проверить,
+        //есть ли уже такой ключ,
+        //поэтому мы проверяем значение по умолчанию (все биты нулевые)
+        if(files[fileHash].timestamp == 0)
+        {
+            files[fileHash] = FileDetails(block.timestamp, owner);
+            //мы создаем событие, благодаря которому программа-оболочка
+            //узнает от контракта, что наличие файла
+            //и принадлежности сохранено
+            emit logFileAddedStatus(true, block.timestamp, owner, fileHash);
+        }
+        else
+        {
+            //здесь мы сообщаем программе-оболочке, что
+            //подтверждение существования
+            //и принадлежности не могут быть сохранены, потому
+            //что данные файла
+            //уже были сохранены раньше
+            emit logFileAddedStatus(false, block.timestamp, owner, fileHash);
+        }
+    }
+
+    //функция для получения сведений о файле
+    function get(string memory fileHash) public returns (uint timestamp, string memory owner)
+    {
+        return (files[fileHash].timestamp, files[fileHash].owner);
     }
 }
